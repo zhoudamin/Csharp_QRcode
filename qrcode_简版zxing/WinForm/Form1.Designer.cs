8afaa9275757ae12c6358d8c3d5defceb95d28ae
DarminZhou <790471332@qq.com>
2017-05-11 17:46:54 +0800

    C#平台的qrcode开发项目源代码，是基于zxing的简化版本，便于研究二维码算法和改进！
    C#平台的qrcode开发项目源代码，是基于zxing的简化版本，便于研究二维码算法和改进！

---
+++ qrcode_简版zxing/WinForm/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.lbshow = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 150);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(363, 302);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(12, 35);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(363, 68);
+            this.txtMsg.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "内容";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "生成条形码";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(107, 119);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "生成二维码";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(300, 119);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "解码";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(259, 119);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(35, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "……";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // lbshow
+            // 
+            this.lbshow.ForeColor = System.Drawing.Color.Red;
+            this.lbshow.Location = new System.Drawing.Point(15, 459);
+            this.lbshow.Name = "lbshow";
+            this.lbshow.Size = new System.Drawing.Size(360, 46);
+            this.lbshow.TabIndex = 6;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 504);
+            this.Controls.Add(this.lbshow);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "1D/2D测试";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label lbshow;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ qrcode_简版zxing/WinForm/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+using com.google.zxing;
+using ByteMatrix = com.google.zxing.common.ByteMatrix;
+
+namespace WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        //生成条形码
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lbshow.Text = "";
+            Regex rg = new Regex("^[0-9]{13}$");
+            if (!rg.IsMatch(txtMsg.Text))
+            {
+                MessageBox.Show("本例子采用EAN_13编码，需要输入13位数字");
+                return;
+            }
+           
+            try
+            {
+                MultiFormatWriter mutiWriter = new com.google.zxing.MultiFormatWriter();
+                ByteMatrix bm = mutiWriter.encode(txtMsg.Text, com.google.zxing.BarcodeFormat.EAN_13, 363, 150);
+                Bitmap img= bm.ToBitmap();
+                pictureBox1.Image =img;
+
+                //自动保存图片到当前目录
+                string filename = System.Environment.CurrentDirectory + "\\EAN_13" + DateTime.Now.Ticks.ToString() + ".jpg";
+                img.Save(filename, System.Drawing.Imaging.ImageFormat.Jpeg);
+                lbshow.Text = "图片已保存到：" + filename;
+            }
+            catch(Exception ee)
+            { MessageBox.Show(ee.Message); }
+        }
+
+        //生成二维码
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lbshow.Text = "";
+            try
+            {
+
+
+//***************************************主程序***********************************************************************************************//、
+
+                MultiFormatWriter mutiWriter = new com.google.zxing.MultiFormatWriter();
+                ByteMatrix bm = mutiWriter.encode(txtMsg.Text, com.google.zxing.BarcodeFormat.QR_CODE, 300, 300);
+                Bitmap img = bm.ToBitmap();
+                pictureBox1.Image = img;
+
+                //自动保存图片到当前目录
+                string filename = System.Environment.CurrentDirectory + "\\QR" + DateTime.Now.Ticks.ToString() + ".jpg";
+                img.Save(filename, System.Drawing.Imaging.ImageFormat.Jpeg);
+                lbshow.Text = "图片已保存到：" + filename;
+            }
+            catch (Exception ee)
+            { MessageBox.Show(ee.Message); }
+        }
+
+        string opFilePath = "";
+        //打开文件
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //openFileDialog1.Filter = "*.jpg|*.jpg";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                opFilePath = openFileDialog1.FileName;
+                pictureBox1.ImageLocation = opFilePath;
+            }
+        }
+
+        //解码操作
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MultiFormatReader mutiReader = new com.google.zxing.MultiFormatReader();
+            Bitmap img = (Bitmap)Bitmap.FromFile(opFilePath);
+            if (img == null)
+                return;
+            LuminanceSource ls = new RGBLuminanceSource(img, img.Width, img.Height);
+            BinaryBitmap bb = new BinaryBitmap(new com.google.zxing.common.HybridBinarizer(ls));
+
+            Hashtable hints = new Hashtable();
+            hints.Add(EncodeHintType.CHARACTER_SET, "UTF-8");
+            Result r = mutiReader.decode(bb, hints);
+            txtMsg.Text = r.Text;
+        }
+    }
+}
